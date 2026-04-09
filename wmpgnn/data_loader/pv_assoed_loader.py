028ed2acdc6d327773221757329d43de890fd246
Yukai Zhao <yukaiz@lxplus986.cern.ch>
2026-04-09 13:56:54 +0200

    changed threadpool to pool due to more cpu than data io only

--- wmpgnn/data_loader/pv_assoed_loader.py
+++ wmpgnn/data_loader/pv_assoed_loader.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 import yaml
 import glob
 
-from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
 from functools import partial
 import threading
 
@@ -162,48 +162,45 @@ def get_trn_val_loaders(configs):
     trn_dataset = []
     val_dataset = []
     weights = {}
-    for sample, files in nfiles.items():
-        # Training
-        nevts["training"][sample] = 0
-        trn_paths = sorted(glob.glob(f'{data_dir}/{sample}/trn_data_*'))[:files]
-        with ThreadPool(processes=ncpus) as pool:
+    with Pool(processes=ncpus) as pool:
+        for sample, files in nfiles.items():
+            # Training
+            nevts["training"][sample] = 0
+            trn_paths = sorted(glob.glob(f'{data_dir}/{sample}/trn_data_*'))[:files]
             results = list(tqdm(pool.imap(load_train_dataset, trn_paths), total=len(trn_paths),
                                 desc=f"Loading {sample} training dataset"))
-        for r in results:
-            trn_dataset.extend(r[0])
-            for key, value in r[1].items():
-                weights[key] = weights.get(key, 0) + value
-            nevts["training"][sample] += len(r[0])
-        # Validation
-        nevts["validation"][sample] = 0
-        val_paths = sorted(glob.glob(f'{data_dir}/{sample}/val_data_*'))[:files]
-        with ThreadPool(processes=ncpus) as pool:
+            for r in results:
+                trn_dataset.extend(r[0])
+                for key, value in r[1].items():
+                    weights[key] = weights.get(key, 0) + value
+                nevts["training"][sample] += len(r[0])
+            # Validation
+            nevts["validation"][sample] = 0
+            val_paths = sorted(glob.glob(f'{data_dir}/{sample}/val_data_*'))[:files]
             results = list(tqdm(pool.imap(load_val_dataset, val_paths), total=len(val_paths),
                                 desc=f"Loading {sample} validation dataset"))
-        for r in results:
-            val_dataset.extend(r)
-            nevts["validation"][sample] += len(r)
-
-    if _configs["settings"].get("domain_adapt"):
-        da_datadir = _configs["settings"]["da_data_dir"]
-        da_nfiles = get_nfiles(_configs["settings"], prefix="da_")
-        for sample, files in da_nfiles.items():
-            # Training, dont calculate weights -> load_val_dataset
-            trn_paths = sorted(glob.glob(f'{da_datadir}/{sample}/trn_data_*'))[:files]
-            with ThreadPool(processes=ncpus) as pool:
-                results = list(tqdm(pool.imap(load_val_dataset, trn_paths), total=len(trn_paths),
-                                    desc=f"Loading {sample} training dataset (da)"))
-            for r in results:
-                trn_dataset.extend(r)
-                nevts["training"][sample] += len(r)
-            # Validation
-            val_paths = sorted(glob.glob(f'{da_datadir}/{sample}/val_data_*'))[:files]
-            with ThreadPool(processes=ncpus) as pool:
-                results = list(tqdm(pool.imap(load_val_dataset, val_paths), total=len(val_paths),
-                                    desc=f"Loading {sample} validation dataset (da)"))
             for r in results:
                 val_dataset.extend(r)
                 nevts["validation"][sample] += len(r)
+
+        if _configs["settings"].get("domain_adapt"):
+            da_datadir = _configs["settings"]["da_data_dir"]
+            da_nfiles = get_nfiles(_configs["settings"], prefix="da_")
+            for sample, files in da_nfiles.items():
+                # Training, dont calculate weights -> load_val_dataset
+                trn_paths = sorted(glob.glob(f'{da_datadir}/{sample}/trn_data_*'))[:files]
+                results = list(tqdm(pool.imap(load_val_dataset, trn_paths), total=len(trn_paths),
+                                    desc=f"Loading {sample} training dataset (da)"))
+                for r in results:
+                    trn_dataset.extend(r)
+                    nevts["training"][sample] += len(r)
+                # Validation
+                val_paths = sorted(glob.glob(f'{da_datadir}/{sample}/val_data_*'))[:files]
+                results = list(tqdm(pool.imap(load_val_dataset, val_paths), total=len(val_paths),
+                                    desc=f"Loading {sample} validation dataset (da)"))
+                for r in results:
+                    val_dataset.extend(r)
+                    nevts["validation"][sample] += len(r)
 
     print(f"Train dataset       : {len(trn_dataset)}")
     print(f"Validation dataset  : {len(val_dataset)}")
