d72f52d9e6e8a0b3557ef115f0d324e6e2f9fee1
Yukai Zhao <yukaiz@lxplus985.cern.ch>
2026-04-16 17:12:16 +0200

    added data loader class instead of only function

---
+++ wmpgnn/data_loader/data_loader_class.py
@@ -0,0 +1,105 @@
+import copy
+from itertools import chain
+
+from torch_geometric.loader import DataLoader
+
+from wmpgnn.util.pv_association import pv_associate_data
+from wmpgnn.util.hetero_data_matching import unify_heterodata
+from wmpgnn.calibration.calibration_mask import *
+from wmpgnn.data_loader.weights_calculator import get_hetero_weight
+from wmpgnn.data_loader.helper import *
+from wmpgnn.calibration.calibration_mask import *
+
+from wmpgnn.util.pruners import *
+
+
+class DataSetLoader():
+    def __init__(self, configs, pv_model=None, ex_graph=None):
+        self.configs = configs
+        self.ex_graph = ex_graph
+        self.pv_model = pv_model
+        # here we add stuff for cuts
+
+        # if cuts exit load normalization
+        self.norm = None
+        if configs["settings"].get("cuts", False):  # TODO: right now it is just done manually
+            self.norm = obtain_normalization(configs)
+
+        self.ncpus = int(configs["settings"]["ncpu"] / 2)
+
+        self.edge_types = [("tracks", "to", "tracks"), ("tracks", "to", "pvs")]
+
+    def load_data(self, path, mode="train"):
+        data = load_file(path)
+
+        """Applying pruning for different using truth pruning initially"""
+        if "true" in self.configs["settings"]["graph_mode"]:
+            data = initial_pruning(data, self.configs["settings"])
+
+        """Apply cuts on the graph"""
+        # apply cut currently done manually TODO: change this
+        for evt in data:
+            ghost_selbool = evt["tracks"].x.T[-1] < 0.6
+            angle_selbool = evt[("tracks", "tracks")].edges[:, 1] < 0.0005
+            if torch.sum(angle_selbool) != 0:
+                indx = evt[("tracks", "tracks")].edge_index[:, angle_selbool]
+                for j in indx.T:
+                    ghost_selbool[j[torch.argmax(evt["tracks"].x[j].T[-1])].item()] = False
+            true_node_pruning(ghost_selbool, evt, "tracks", self.edge_types)
+
+        """Making the graph bidirectional"""
+        for evt in data:
+            edge_type = ('tracks', 'to', 'tracks')
+            store = evt[edge_type]
+
+            store.edge_index = torch.cat([store.edge_index, store.edge_index.flip(0)], dim=1)
+            store.edges = store.edges.repeat(2, 1)  # More efficient than cat([x]*2)
+            if hasattr(store, 'y') and store.y is not None:
+                store.y = store.y.repeat(2)
+            if hasattr(store, 'lca') and store.lca is not None:
+                store.lca = store.lca.repeat(2, 1)
+            if hasattr(store, 'pred_y') and store.lca is not None:
+                store.pred_y = store.pred_y.repeat(2)
+
+        """PV association"""
+        if self.pv_model is not None:
+            pv_data = DataLoader(data, batch_size=512)
+            data = []
+            for evt in pv_data:
+                if pv_asso_model.name == "pv_asso_module":
+                    original_data = copy.deepcopy(evt)
+                    metrics = pv_asso_model.forward(evt)
+                    res = pv_associate_data(original_data, metrics, node_thr=pv_asso_model.node_thrs,
+                                            n_cores=self.ncpus)
+                else:
+                    metrics = pv_asso_model.forward(evt)
+                    res = pv_associate_data(evt, metrics, n_cores=self.ncpus)
+                data.append(res)
+            data = list(chain.from_iterable(data))
+
+        """Whitening for calibration"""
+        if self.configs["settings"]["calibration"]:
+            data = adjust_for_calibration(configs, path, data, n_cores=self.ncpus)
+
+        """Domain adaptation labeling"""
+        if self.configs["settings"]["domain_adapt"]:
+            if self.configs["settings"]["da_data_dir"] in path:
+                if 'tst' not in path:
+                    label = torch.tensor([1.], dtype=torch.float32)  # data label
+                else:
+                    label = torch.tensor([0.], dtype=torch.float32)  # set to 0 to run dfei during testing
+            else:
+                label = torch.tensor([0.], dtype=torch.float32)  # MC label
+            for evt in data:
+                evt["da_label"] = label
+                if ex_graph is not None:
+                    unify_heterodata(evt, ex_graph)  # storage within graph which do not exist are padded with 0
+
+        if mode == "weights_only":
+            weights = get_hetero_weight(data, configs)
+            return weights
+        elif "weights" in mode:
+            weights = get_hetero_weight(data, configs)
+            return data, weights
+        else:
+            return data
--- wmpgnn/data_loader/helper.py
+++ wmpgnn/data_loader/helper.py
@@ -1,16 +1,9 @@
-import copy
 import io
 import zstandard as zstd
-from itertools import chain
 
 import torch
-from torch_geometric.loader import DataLoader
 
 from wmpgnn.util.pruners import *
-from wmpgnn.util.pv_association import pv_associate_data
-from wmpgnn.util.hetero_data_matching import unify_heterodata
-from wmpgnn.calibration.calibration_mask import *
-from wmpgnn.data_loader.weights_calculator import get_hetero_weight
 
 
 def get_nfiles(_configs, prefix=""):
@@ -24,91 +17,26 @@ def load_file(path):
     dctx = zstd.ZstdDecompressor()
     with open(path, 'rb') as f:
         with dctx.stream_reader(f) as reader:
-            # Read all decompressed data into memory
             decompressed = reader.read()
-            # Load from BytesIO buffer
             data = torch.load(io.BytesIO(decompressed), weights_only=False)
     return data
 
 
-def load_dataset(path, configs, mode="train", pv_asso_model=None, ex_graph=None):
-    data = load_file(path)
-
-    """Applying pruning for different using truth pruning initially"""
-    if "true" in configs["settings"]["graph_mode"]:  # here we need to apply the pv association as well
-        data_selbool = torch.ones(len(data))
-        edge_types = [("tracks", "to", "tracks"), ("tracks", "to", "pvs")]
-        for i, evt in enumerate(data):
-            y_nodes = evt["tracks"].ft != 1  # 0 bbar 2 b
-            if "frag" in configs["settings"]["graph_mode"]:
-                frag_selbool = evt["tracks"].frag != 0
-                y_nodes = y_nodes | frag_selbool
-            if configs["settings"]["node_sel"] == "true":
-                true_node_pruning(y_nodes, evt, "tracks", edge_types)
-            elif configs["settings"]["node_sel"] == "default":
-                node_pruning(y_nodes, evt, "tracks", edge_types)
-            elif configs["settings"]["node_sel"] == "zeros":
-                test_node_pruning(y_nodes, evt, "tracks", edge_types)
-            if evt[("tracks", "to", "tracks")].y.shape[0] == 0 or torch.all(evt[("tracks", "to", "tracks")].y == 0):
-                data_selbool[i] = 0
-        filtered_data = [d for d, sel in zip(data, data_selbool) if sel]
-    else:
-        filtered_data = data
-
-    """Making the graph bidirectional"""
-    for data in filtered_data:
-        edge_type = ('tracks', 'to', 'tracks')
-        store = data[edge_type]
-
-        store.edge_index = torch.cat([store.edge_index, store.edge_index.flip(0)], dim=1)
-        store.edges = store.edges.repeat(2, 1)  # More efficient than cat([x]*2)
-        if hasattr(store, 'y') and store.y is not None:
-            store.y = store.y.repeat(2)
-        # if exists pred y and lca on edges as well
-        if hasattr(store, 'lca') and store.lca is not None:
-            store.lca = store.lca.repeat(2, 1)
-        if hasattr(store, 'pred_y') and store.lca is not None:
-            store.pred_y = store.pred_y.repeat(2)
-
-    """PV asso"""
-    if pv_asso_model is not None:
-        ncpus = int(configs["settings"]["ncpu"] / 2)
-        pv_data = DataLoader(filtered_data, batch_size=200)
-        filtered_data = []
-        for evt in pv_data:
-            if pv_asso_model.name == "pv_asso_module":
-                original_data = copy.deepcopy(evt)
-                metrics = pv_asso_model.forward(evt)
-                res = pv_associate_data(original_data, metrics, node_thr=pv_asso_model.node_thrs, n_cores=ncpus)
-            else:
-                metrics = pv_asso_model.forward(evt)
-                res = pv_associate_data(evt, metrics, n_cores=ncpus)
-            filtered_data.append(res)
-        filtered_data = list(chain.from_iterable(filtered_data))
-
-    """Whitening for calibration"""
-    if configs["settings"]["calibration"]:
-        filtered_data = adjust_for_calibration(configs, path, filtered_data,
-                                               n_cores=int(configs["settings"]["ncpu"] / 2))
-    """Domain adaptation labeling"""
-    if configs["settings"]["domain_adapt"]:
-        if configs["settings"]["da_data_dir"] in path:
-            if 'tst' not in path:
-                label = torch.tensor([1.], dtype=torch.float32)  # data label
-            else:
-                label = torch.tensor([0.], dtype=torch.float32)  # set to 0 to run dfei during testing
-        else:
-            label = torch.tensor([0.], dtype=torch.float32)  # MC label
-        for evt in filtered_data:
-            evt["da_label"] = label
-            if ex_graph is not None:
-                unify_heterodata(evt, ex_graph)  # storage within graph which do not exist are padded with 0
-
-    if mode == "weights_only":
-        weights = get_hetero_weight(filtered_data, configs)
-        return weights
-    elif "weights" in mode:
-        weights = get_hetero_weight(filtered_data, configs)
-        return filtered_data, weights
-    else:
-        return filtered_data
+def initial_pruning(data, configs):
+    data_selbool = torch.ones(len(data))
+    edge_types = [("tracks", "to", "tracks"), ("tracks", "to", "pvs")]
+    for i, evt in enumerate(data):
+        y_nodes = evt["tracks"].ft != 1  # 0 bbar 2 b
+        if "frag" in configs["settings"]["graph_mode"]:
+            frag_selbool = evt["tracks"].frag != 0
+            y_nodes = y_nodes | frag_selbool
+        if configs["settings"]["node_sel"] == "true":
+            true_node_pruning(y_nodes, evt, "tracks", edge_types)
+        elif configs["settings"]["node_sel"] == "default":
+            node_pruning(y_nodes, evt, "tracks", edge_types)
+        elif configs["settings"]["node_sel"] == "zeros":
+            test_node_pruning(y_nodes, evt, "tracks", edge_types)
+        if evt[("tracks", "to", "tracks")].y.shape[0] == 0 or torch.all(evt[("tracks", "to", "tracks")].y == 0):
+            data_selbool[i] = 0
+    filtered_data = [d for d, sel in zip(data, data_selbool) if sel]
+    return filtered_data
