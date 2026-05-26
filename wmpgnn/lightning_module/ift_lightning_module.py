9b4cf3ea210882ceeeb26c71003d73fd79958d1a
Yukai Zhao <yukaiz@lxplus995.cern.ch>
2026-05-26 16:15:00 +0200

    shared step only for trn and val

--- wmpgnn/lightning_module/ift_lightning_module.py
+++ wmpgnn/lightning_module/ift_lightning_module.py
@@ -33,35 +33,55 @@ class IFTLightningModule(L.LightningModule):
 
         self.configs = configs["inference"]
         self.model = model
-
-        self.dfei_model = [dfei_model]  # doesnt get saved in ckpt
+        # DFEI model pass within IFT
+        self.dfei_model = [dfei_model]
         if self.dfei_model[0] is not None:
             for param in self.dfei_model[0].parameters():
                 param.requires_grad = False
-        self.dfei_use_pid = configs['DFEI']["use_pid"] if "DFEI" in configs else "None"
+        self.dfei_use_pid = configs['DFEI']["use_pid"] if "DFEI" in configs else False
         self.ift_use_pid = configs["IFT"]["use_pid"]
 
         self.optimizer_class = optimizer_class
         self.optimizer_params = optimizer_params
 
+        # Loss functions + associated inference class for plotting
         if self.configs["frag"]:
             self.frag_criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weights["frag"])
         if self.configs["FT"]:
             self.ft_criterion = nn.CrossEntropyLoss(weight=pos_weights["FT"])
 
-        self.trn_log, self.val_log = init_logs(configs)
-        self.tst_log = init_logs(configs, mode="test")
-        # init event reconstruction class
-        self.evt_reco = EventReconstruction(configs)
-
-        # Pruning threshold for reco
-        self.edge_prune = configs["inference"]["edge_prune_thr"]
-        self.node_prune = configs["inference"]["node_prune_thr"]
-
+        # init log directories and event reconstruction class
         self.log_dir = configs["log_dir"]
+        (self.trn_log, self.val_log), self.tst_log = init_logs(configs), init_logs(configs, mode="test")
+        self.evt_reco = EventReconstruction(configs)
+        self.tst_mode = 'MC'
+
 
     def forward(self, batch):
-        return self.model(batch)
+        # Adding lca information to edges
+        if self.dfei_model[0] is not None:  # lca from dfei model, overwrites from pv asso
+            dfei_input = copy.deepcopy(batch)
+            if self.dfei_use_pid:
+                dfei_input["tracks"].x = torch.cat([dfei_input["tracks"].x, dfei_input["tracks"].pid], dim=1)
+            self.dfei_model[0] = self.dfei_model[0].to(self.device)
+            dfei_outputs = self.dfei_model[0](dfei_input)
+            lca = dfei_outputs[("tracks", "to", "tracks")].edges
+        elif "lca" in batch[("tracks", "to", "tracks")]:  # using the information from the pv asso model
+            lca = batch[("tracks", "to", "tracks")].lca
+        else:  # using truth information
+            labels = batch[("tracks", "tracks")].y.to(torch.long)
+            lca = torch.nn.functional.one_hot(labels, num_classes=4).float()
+
+        lca_score = torch.argmax(lca, dim=1).unsqueeze(1)
+        batch[("tracks", "tracks")].edges = torch.cat([batch[("tracks", "tracks")].edges, lca_score], dim=1)
+        batch["tracks"].x = torch.cat([batch["tracks"].x, batch["tracks"].pred_y.unsqueeze(dim=1)], dim=1)
+
+        if self.ift_use_pid:
+            batch["tracks"].x = torch.cat([batch["tracks"].x, batch["tracks"].pid], dim=1)
+
+        output = self.model(batch)
+        output[("tracks", "to", "tracks")].lca = lca
+        return output
 
     def configure_optimizers(self):
         return self.optimizer_class(self.model.parameters(), **self.optimizer_params)
@@ -70,71 +90,13 @@ class IFTLightningModule(L.LightningModule):
         optimizers = self.optimizers()
         loss = init_loss(self.device)
 
-        # Adding lca information to edges
-        if self.dfei_model[0] is not None:  # lca from dfei model, overwrites from pv asso
-            dfei_input = copy.deepcopy(batch)
-            if self.dfei_use_pid == "realistic":
-                dfei_input["tracks"].x = torch.cat([dfei_input["tracks"].x, dfei_input["tracks"].real_pid], dim=1)
-            elif self.dfei_use_pid == "true":
-                dfei_input["tracks"].x = torch.cat([dfei_input["tracks"].x, dfei_input["tracks"].pid], dim=1)
-            self.dfei_model[0] = self.dfei_model[0].to(self.device)
-            dfei_outputs = self.dfei_model[0](dfei_input)
-            lca = dfei_outputs[("tracks", "to", "tracks")].edges
-        elif "lca" in batch[("tracks", "to", "tracks")]:  # using the information from the pv asso model
-            lca = batch[("tracks", "to", "tracks")].lca
-        else:  # using truth information
-            lca = torch.nn.functional.one_hot(batch[("tracks", "tracks")].y.to(torch.long), num_classes=4).to(
-                torch.float32)
-        lca_score = torch.argmax(lca, dim=1).unsqueeze(1)
-        batch[("tracks", "tracks")].edges = torch.cat([batch[("tracks", "tracks")].edges, lca_score], dim=1)
-
-        # Adding pid information to nodes, here again realistic or not
-        if self.ift_use_pid == "realistic":
-            batch["tracks"].x = torch.cat([batch["tracks"].x, batch["tracks"].real_pid], dim=1)
-        elif self.ift_use_pid == "true":
-            batch["tracks"].x = torch.cat([batch["tracks"].x, batch["tracks"].pid], dim=1)
-
-        # Adding y pred score
-        batch["tracks"].x = torch.cat([batch["tracks"].x, batch["tracks"].pred_y.unsqueeze(dim=1)], dim=1)
-
-        # Forward pass of IFt model
-        outputs_ft = self.model(batch)
-        outputs_ft[("tracks", "to", "tracks")].lca = lca
+        # Forward pass
+        outputs_ft = self.forward(batch)
 
         y_ft = batch['tracks'].ft
         selbool = y_ft != 1  # this is actually a good question if one should use ft truth or the predicted from DFEI
         ift_loss = self.ft_criterion(outputs_ft["tracks"].x[selbool], y_ft[selbool])
         loss["ft_nodes"] += ift_loss
-
-        # Starting evaluating the performance
-        if mode == "test":
-            ft_des = torch.softmax(outputs_ft["tracks"].x, dim=1)
-            if "frag" in outputs_ft["tracks"]:
-                frag_selbool = outputs_ft["tracks"].frag != -1  # this does not need to exist
-                frag_in_evt = outputs_ft["tracks"].frag[frag_selbool]
-                frag_pid = outputs_ft["part_ids"][frag_selbool]
-
-            # Attach reco information to graph
-            # Node weights
-            if self.dfei_model[0] is not None:
-                outputs_ft["node_weights"] = self.dfei_model[0]._blocks[-1].node_weights["tracks"].squeeze()
-            elif "pred_y" in outputs_ft["tracks"]:
-                outputs_ft["node_weights"] = outputs_ft["tracks"].pred_y
-            else:
-                outputs_ft["node_weights"] = (outputs_ft["tracks"].ft != 1).int()
-            # Edge weights
-            if self.dfei_model[0] is not None:
-                outputs_ft["edge_weights"] = self.dfei_model[0]._blocks[-1].edge_weights[
-                    ('tracks', 'to', 'tracks')].squeeze()
-            elif "pred_y" in outputs_ft[("tracks", "tracks")]:
-                outputs_ft["edge_weights"] = outputs_ft[("tracks", "tracks")].pred_y
-            else:
-                outputs_ft["edge_weights"] = (lca_score.squeeze() != 0).int()
-
-            if "frag" in outputs_ft["tracks"]:  # how in the world does this make sense?
-                outputs_ft["frag_y"] = frag_in_evt
-                outputs_ft["frag_pid"] = frag_pid
-            self.evt_reco.reconstruct_heavyhadrons(outputs_ft, ft_des=ft_des)
 
         log = loss_logging(log, loss, self.configs, mode="IFT")
         return ift_loss
@@ -148,7 +110,44 @@ class IFTLightningModule(L.LightningModule):
         return loss
 
     def test_step(self, batch, batch_idx):
-        _ = self.shared_step(batch, batch_idx, self.tst_log, mode="test")
+        org_x = batch["tracks"].x.clone()
+        org_pid = batch["tracks"].pid.clone()
+
+        # Forward pass
+        outputs_ft = self.forward(batch)
+
+        outputs_ft['tracks'].org_x = org_x
+        outputs_ft['tracks'].org_pid = org_pid
+
+        # Flavour tag decision
+        ft_des = torch.softmax(outputs_ft["tracks"].x, dim=1)
+        # Node decisions
+        if self.dfei_model[0] is not None:
+            outputs_ft["node_weights"] = self.dfei_model[0]._blocks[-1].node_weights["tracks"].squeeze()
+        elif "pred_y" in outputs_ft["tracks"]:
+            outputs_ft["node_weights"] = outputs_ft["tracks"].pred_y
+        else:
+            outputs_ft["node_weights"] = (outputs_ft["tracks"].ft != 1).int()
+        # Edge decisions
+        if self.dfei_model[0] is not None:
+            outputs_ft["edge_weights"] = self.dfei_model[0]._blocks[-1].edge_weights[
+                ('tracks', 'to', 'tracks')].squeeze()
+        elif "pred_y" in outputs_ft[("tracks", "tracks")]:
+            outputs_ft["edge_weights"] = outputs_ft[("tracks", "tracks")].pred_y
+        else:
+            outputs_ft["edge_weights"] = (lca_score.squeeze() != 0).int()
+
+
+        """if "frag" in outputs_ft["tracks"]:
+            frag_selbool = outputs_ft["tracks"].frag != -1  # this does not need to exist
+            frag_in_evt = outputs_ft["tracks"].frag[frag_selbool]
+            frag_pid = outputs_ft["part_ids"][frag_selbool]
+            outputs_ft["frag_y"] = frag_in_evt
+            outputs_ft["frag_pid"] = frag_pid"""
+
+        self.evt_reco.reconstruct_heavyhadrons(outputs_ft, ft_des=ft_des)
+
+
         return {}
 
     def on_train_epoch_end(self):
@@ -170,16 +169,6 @@ class IFTLightningModule(L.LightningModule):
         sig_df.to_csv(f'{self.log_dir}/IFT/version_{self.version}/signal_reco_df_{self.signal}.csv', index=False)
         evt_df.to_csv(f'{self.log_dir}/IFT/version_{self.version}/event_reco_df_{self.signal}.csv', index=False)
         obtain_reco_accuracy(sig_df, self.version, self.signal, self.log_dir, model="IFT")
-        """# Removing heavy hadron daughters of B since they are classified as signal (Ds in Bs->Dspi for example)
-        if "Bs" in self.signal:
-            sig_id = 531
-        elif "Bd" in self.signal:
-            sig_id = 511
-        else:
-            ValueError("Currently undefined")
-        sig_selbool = sig_df["SigMatch"] == 1
-        sig_id_selbool = np.abs(sig_df["B_id"]) != sig_id
-        sig_df = sig_df[~(sig_selbool * sig_id_selbool)]"""
         if self.configs["FT"]:
             process_ft(self.tst_log, sig_df, self.version, self.signal, log_dir=self.log_dir)
             analyze_tagging_power(sig_df, self.version, self.signal, log_dir=self.log_dir)
